7287adbb587737e04c9478268d82f3e404988723
Jorge Sandoval <jorgesagt5@gmail.com>
2026-02-03 21:45:55 -0700

    Se implementaron links HATEOAS en los enpoints REST de Employee

--- src/main/java/com/jorge/Payroll/Controller/EmployeeController.java
+++ src/main/java/com/jorge/Payroll/Controller/EmployeeController.java
@@ -3,9 +3,15 @@ package com.jorge.Payroll.Controller;
 import com.jorge.Payroll.Entiy.Employee;
 import com.jorge.Payroll.Exception.EmployeeNotFoundException;
 import com.jorge.Payroll.Respository.EmployeeRepository;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
@@ -18,8 +24,15 @@ public class EmployeeController {
     // Aggregate root
     //tag::get-aggragate-root[]
     @GetMapping("/employees")
-    List<Employee> all(){
-        return repository.findAll();
+    CollectionModel<EntityModel<Employee>> all(){
+
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(employee -> EntityModel.of(employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     // end::get-aggrwgate-root[]
@@ -32,9 +45,14 @@ public class EmployeeController {
     //Single item
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return repository.findById(id)
+    EntityModel<Employee> one(@PathVariable Long id) {
+
+        Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
