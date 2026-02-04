30f4b8700cab415f9a407ff26cbc543ed7d3cc0b
Jorge Sandoval <jorgesagt5@gmail.com>
2026-02-03 22:05:23 -0700

    Se implementó la interface EmployeeModelAssembler para reducir el código repetitivo

---
+++ src/main/java/com/jorge/Payroll/Assembler/EmployeeModelAssembler.java
@@ -0,0 +1,21 @@
+package com.jorge.Payroll.Assembler;
+
+import com.jorge.Payroll.Controller.EmployeeController;
+import com.jorge.Payroll.Entiy.Employee;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+    @Override
+    public EntityModel<Employee> toModel(Employee employee) {
+        return EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employee"));
+    }
+}
